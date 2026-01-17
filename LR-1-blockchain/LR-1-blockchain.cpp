2f7fdb1d60558d4cccdc0804c898b4b9adf8197b
IvanovMI <ivanov-mi-98@yandex.ru>
2026-01-17 16:43:43 +0500

    Добавьте файлы проекта.

---
+++ LR-1-blockchain/LR-1-blockchain.cpp
@@ -0,0 +1,27 @@
+﻿//Демонстрация ошибки race condition.
+
+#include <iostream>
+#include <thread>
+
+static long long counter = 0;
+
+void inc(long long n) {
+    for (long long i = 0; i < n; ++i) {
+        // НЕатомарная операция: read-modify-write
+        counter++;
+    }
+}
+
+int main() {
+    const long long N = 10'000'000;
+
+    std::thread t1(inc, N);
+    std::thread t2(inc, N);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Expected: " << (2 * N) << "\n";
+    std::cout << "Actual:   " << counter << "\n";
+    return 0;
+}

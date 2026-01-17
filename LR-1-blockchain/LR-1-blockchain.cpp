287efb754c2343e1a5783e2b4c5a05cb22c87a67
IvanovMI <ivanov-mi-98@yandex.ru>
2026-01-17 16:49:04 +0500

    Добавлено исправление через mutex

--- LR-1-blockchain/LR-1-blockchain.cpp
+++ LR-1-blockchain/LR-1-blockchain.cpp
@@ -1,5 +1,5 @@
 ﻿//Демонстрация ошибки race condition.
-
+/*
 #include <iostream>
 #include <thread>
 
@@ -8,6 +8,37 @@ static long long counter = 0;
 void inc(long long n) {
     for (long long i = 0; i < n; ++i) {
         // НЕатомарная операция: read-modify-write
+        counter++;
+    }
+}
+
+int main() {
+    const long long N = 10'000'000;
+
+    std::thread t1(inc, N);
+    std::thread t2(inc, N);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Expected: " << (2 * N) << "\n";
+    std::cout << "Actual:   " << counter << "\n";
+    return 0;
+}
+*/
+
+
+//Исправление ошибки через mutex
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+static long long counter = 0;
+static std::mutex m;
+
+void inc(long long n) {
+    for (long long i = 0; i < n; ++i) {
+        std::lock_guard<std::mutex> lock(m);
         counter++;
     }
 }
